9e45f2f0a1ce57a2d57b222914e997faf4497f0a
kingmui <muikinghk@yahoo.com.hk>
2017-10-22 23:14:46 +0800

    Initial commit

--- CarouselFigureView/tweenAnimationsVersion/js/index.js
+++ CarouselFigureView/tweenAnimationsVersion/js/index.js
@@ -1,6 +1,7 @@
 ;(function(){
 	var banner = document.querySelector('.banner');
 	var ul = banner.querySelector('ul');
+	var indicators = banner.querySelectorAll('ol>li');
 	var lis = ul.children;
 	var liWidth = 0;
 	var ulWidth = 0;
@@ -8,32 +9,83 @@
 	var startTime = 0;
 	var count = 1;
 	var timer;
+	var Carousel = {};
+	Carousel.move = function(){
+		// 定时器函数封装
+		timer = setInterval(function(){
+			count++;
+			Carousel.addTransition(ul);
+			Carousel.setTranslate(ul,- liWidth * count);
+		},2000);
+	}
+	Carousel.boundaryJudgment = function(){
+		// 边界值判断
+		if(count >= lis.length -1){
+			count = 1;
+		}
+		if(count <= 0){
+			count = lis.length - 2;
+		}
+	}
+	Carousel.addTransition = function(ele){
+		// 添加过度
+		ele.style.transition = "all .5s";
+		ele.style.webkitTransition = "all .5s";
+	}
+	Carousel.removeTransition = function(ele){
+		// 移除过度
+		ele.style.transition = "none";
+		ele.style.webkitTransition = "none";
+	}
+	Carousel.setTranslate = function(ele,value,direction){
+		// 设置变换
+		direction = direction ? direction : "X";
+		ele.style.transform = "translate" + direction + "(" + value +"px)";
+	}
+	Carousel.setUlWidth = function(){
+		// 设置ul标签宽的函数封装
+		liWidth = banner.offsetWidth;
+		ulWidth = lis.length * liWidth;
+		for(var i = 0; i < lis.length; i++){
+			lis[i].style.width = liWidth + "px";
+		}
+		ul.style.width = ulWidth + "px";
+	}
 
 	// 页面加载时：设置ul标签的宽和位置
 	window.addEventListener("load",function(){
-		setUlWidth();
-		setTranslate(ul,- liWidth * count);
+		Carousel.setUlWidth();
+		Carousel.setTranslate(ul,- liWidth * count);
 	});
 
 	// 重置窗口大小时：设置ul标签的宽和位置
 	window.addEventListener("resize",function(){
 		clearInterval(timer);
-		setUlWidth();
-		removeTransition(ul);
-		setTranslate(ul,- liWidth * count);
-		move();
+		Carousel.setUlWidth();
+		Carousel.removeTransition(ul);
+		Carousel.setTranslate(ul,- liWidth * count);
+		Carousel.move();
 	});
+
+	Carousel.move();
 
 	// 过度结束事件
 	ul.addEventListener("transitionend",function(){
 		if(count >= lis.length -1){
 			count = 1;
-			noTransitionMove();
+			Carousel.removeTransition(ul);
+			Carousel.setTranslate(ul,- liWidth * count);
 		}
 		if(count <= 0){
 			count = lis.length - 2;
-			noTransitionMove();
+			Carousel.removeTransition(ul);
+			Carousel.setTranslate(ul,- liWidth * count);
 		}
+		// 同步小圆点
+		for(var i = 0; i < indicators.length; i++){
+			indicators[i].classList.remove("current");
+		}
+		indicators[count - 1].classList.add("current");
 	});
 
 	// 开始触摸
@@ -45,65 +97,23 @@
 
 	// 触摸移动
 	ul.addEventListener("touchmove",function(e){
+		Carousel.boundaryJudgment();
 		var distance = e.changedTouches[0].clientX - startX;
-		removeTransition(ul);
-		setTranslate(ul,- liWidth * count);
+		Carousel.removeTransition(ul);
+		Carousel.setTranslate(ul,-liWidth * count + distance);
 	});
 
 	// 触摸结束
 	ul.addEventListener("touchend",function(e){
+		Carousel.boundaryJudgment();
 		var distance = e.changedTouches[0].clientX - startX;
 		var moveTime = new Date() - startTime;
 		console.log("距离：" + distance + ",时间：" + moveTime);
 		if(Math.abs(distance) >= liWidth / 3 || moveTime <= 300 && Math.abs(distance) >= 30){
 			distance > 0 ? count-- : count++;
 		}
-		addTransition(ul);
-		setTranslate(ul,- liWidth * count);
-		move();
-	})
-
-	move();
-	// 定时器函数封装
-	function move(){
-		timer = setInterval(function(){
-			count++;
-			addTransition(ul);
-			setTranslate(ul,- liWidth * count);
-		},1000);
-	}
-
-	// 添加过度
-	function addTransition(ele){
-		ele.style.transition = "all .3s";
-		ele.style.webkitTransition = "all .3s";
-	}
-
-	// 移除过度
-	function removeTransition(ele){
-		ele.style.transition = "none";
-		ele.style.webkitTransition = "none";
-	}
-
-	// 设置变换
-	function setTranslate(ele,value,direction){
-		direction = direction ? direction : "X";
-		ele.style.transform = "translate" + direction + "(" + value +"px)";
-	}
-
-	// 无过度瞬移
-	function noTransitionMove(){
-		removeTransition(ul);
-		setTranslate(ul,- liWidth * count);
-	}
-
-	// 设置ul标签宽的函数封装
-	function setUlWidth(){
-		liWidth = banner.offsetWidth;
-		ulWidth = lis.length * liWidth;
-		for(var i = 0; i < lis.length; i++){
-			lis[i].style.width = liWidth + "px";
-		}
-		ul.style.width = ulWidth + "px";
-	}
+		Carousel.addTransition(ul);
+		Carousel.setTranslate(ul,- liWidth * count);
+		Carousel.move();
+	});
 })();
