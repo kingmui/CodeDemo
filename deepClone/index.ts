0954edad283cfcfa1cd91662f9ed7f09e7d86ce0
muli <muli@byai.com>
2020-09-10 18:29:16 +0800

    feat: set map 等类型

--- deepClone/index.ts
+++ deepClone/index.ts
@@ -1,3 +1,22 @@
+// 可继续遍历
+const mapTag = "[object Map]";
+const setTag = "[object Set]";
+const arrayTag = "[object Array]";
+const objectTag = "[object Object]";
+const argsTag = "[object Arguments]";
+
+// 不可继续遍历
+const boolTag = "[object Boolean]";
+const dateTag = "[object Date]";
+const numberTag = "[object Number]";
+const stringTag = "[object String]";
+const symbolTag = "[object Symbol]";
+const errorTag = "[object Error]";
+const regexpTag = "[object RegExp]";
+const funcTag = "[object Function]";
+
+const DEEP_TAG = [mapTag, setTag, arrayTag, objectTag, argsTag];
+
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/WeakMap
 // WeakMap 对象是一组键/值对的集合，其中的键是弱引用的。其键必须是对象，而值可以是任意的
 // 持有的是每个键对象的“弱引用”，这意味着在没有其他引用存在时垃圾回收能正确进行
@@ -8,12 +27,39 @@ function deepClone(source, hashMap = new WeakMap()) {
   // 查找哈希表，解决**循环引用**
   if (hashMap.has(source)) return hashMap.get(source);
 
-  const isArray = Array.isArray(source);
+  // 初始化
+  const type = getType(source);
 
-  const target = isArray ? [...source] : { ...source };
+  // const isArray = Array.isArray(source);
+  // const target = isArray ? [...source] : { ...source };
+
+  let target;
+  if (DEEP_TAG.includes(type)) {
+    target = getInit(source);
+  } else {
+    return cloneBaseType(source, type);
+  }
 
   // 设置哈希表
   hashMap.set(source, target);
+
+  // 克隆 Set
+  if (type === setTag) {
+    source.forEach((value) => {
+      target.add(deepClone(value));
+    });
+
+    return target;
+  }
+
+  // 克隆 Map
+  if (type === mapTag) {
+    source.forEach((value, key) => {
+      target.set(key, deepClone(value));
+    });
+
+    return target;
+  }
 
   // *************** Reflect
   // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Reflect/ownKeys
@@ -73,9 +119,9 @@ function deepClone(source, hashMap = new WeakMap()) {
   return target;
 }
 
-function isObject(obj) {
-  return obj !== null && typeof obj === "object";
-  // return Object.prototype.toString.call(obj) === '[object Object]';
+function isObject(target) {
+  const type = typeof target;
+  return target !== null && (type === "object" || type === "function");
 }
 
 function forEach(array: any[], iterator: (value: any, index: number) => void) {
@@ -86,4 +132,42 @@ function forEach(array: any[], iterator: (value: any, index: number) => void) {
   }
 
   return array;
+}
+
+function getType(target) {
+  return Object.prototype.toString.call(target);
+}
+
+function getInit(target) {
+  const Ctor = target.constructor;
+  return new Ctor();
+}
+
+function cloneBaseType(target, type) {
+  const Ctor = target.constructor;
+  switch (type) {
+    case boolTag:
+    case numberTag:
+    case stringTag:
+    case errorTag:
+    case dateTag:
+      return new Ctor(target);
+    case regexpTag:
+      return cloneReg(target);
+    case symbolTag:
+      return cloneSymbol(target);
+    default:
+      return null;
+  }
+}
+
+function cloneSymbol(targe) {
+  return Object(Symbol.prototype.valueOf.call(targe));
+}
+
+function cloneReg(target) {
+  const reFlags = /\w*$/;
+  const result = new target.constructor(target.source, reFlags.exec(target));
+  result.lastIndex = target.lastIndex;
+  return result;
 }
